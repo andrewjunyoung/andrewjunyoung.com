e2f3581e57e6ce3c4945ead7b945ff7126528223
ajy <andrew@getinstant.app>
2022-02-07 01:29:09 -0500

    Pushing main page changes. Todo: show projects on main page.

--- landing_page/src/pages/index.tsx
+++ landing_page/src/pages/index.tsx
@@ -5,21 +5,11 @@ import Header from "@lekoarts/gatsby-theme-emilia/src/components/header"
 import Layout from "@lekoarts/gatsby-theme-emilia/src/components/layout"
 import Seo from "@lekoarts/gatsby-theme-emilia/src/components/seo"
 
-const NotFound = () => (
+const Index = () => (
   <Layout>
-    <Seo title="404 - Not found" />
+    <Seo title="Home page" />
     <Header />
-    <Container>
-      <Themed.p>
-        Oh, no!
-        <br />
-        You found a page that doesn't exist.{` `}
-        <TLink as={Link} to="/">
-          Do you want to visit the homepage?
-        </TLink>
-      </Themed.p>
-    </Container>
   </Layout>
 )
 
-export default NotFound
+export default Index
